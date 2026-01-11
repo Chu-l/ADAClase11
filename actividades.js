34d0ba8cc86beb72a6f71ef54c0b9002c2514c2c
Lilen <lilen.chu@hotmail.com>
2026-01-10 23:01:07 -0300

    Ejercicio 2 terminado

--- actividades.js
+++ actividades.js
@@ -2,7 +2,7 @@ const prompt = require('prompt-sync')();
 
 /*Ejercicio 1: ¿Positivo, negativo o cero?*/
 /*Vamos a trabajar con números y aprender a clasificarlos. Escribe un programa que le pida al usuario un número cualquiera. El programa deberá analizar si el número ingresado es positivo, negativo o cero. Utiliza estructuras if para resolverlo y muestra un mensaje explicativo en cada caso.*/
-
+/*
 let num = Number(prompt("Ingrese un número:"));
 
 if (num>0) {
@@ -12,3 +12,29 @@ if (num>0) {
 } else {
     console.log("El número es cero");
 }
+*/ 
+
+/*Ejercicio 2: Semáforo inteligente*/
+/*Vamos a programar un semáforo. Escribe un programa que le pida al usuario ingresar un color del semáforo: rojo, amarillo o verde. Dependiendo del color ingresado, el programa debe imprimir un mensaje con la acción correspondiente:
+● Rojo: "Alto, no puedes avanzar."
+● Amarillo: "Precaución, prepárate para avanzar."
+● Verde: "Avanza con seguridad." Si el usuario ingresa un color inválido, el programa debe mostrar un mensaje: "Color no reconocido, ingresa rojo, amarillo o verde." Utiliza un switch para resolver este ejercicio.*/
+
+let color = prompt("Ingrese un color del semáforo (rojo, amarillo o verde): ");
+
+color = color.toLowerCase(); //por si el usuario escribe con mayúsculas
+
+switch (color) {
+    case "rojo":
+        console.log("Alto, no puedes avanzar.");
+        break;
+    case "amarillo":
+        console.log("Precaución, prepárate para avanzar.");
+        break;
+    case "verde":
+        console.log("Avanza con seguridad.");
+        break;
+    default:
+        console.log("Color no reconocido, ingresa rojo, amarillo o verde.");
+}
+
